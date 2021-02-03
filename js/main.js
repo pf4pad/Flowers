e2e58f395ebf93021a1da714d82ec7c38c2c0f1f
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-02-03 22:00:02 +0200

    section2

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const flowersSlider = new Swiper('.flowers-slider', {
+  // Optional parameters
+  
+  loop: true,
+
+  slidesPerView: 6,
+
+  // Cnhtkrb
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  
+  loop: true,
+
+  slidesPerView: 1,
+
+  // Cnhtkrb
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});
